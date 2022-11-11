8cd9a841195571c95c5775f3c72d9efa3b29c8a7
Yana Pavlyk <pavlikyg@gmail.com>
2022-11-11 19:47:36 +0200

    Update StyledForm.styled.jsx

--- src/components/Common/StyledForm.styled.jsx
+++ src/components/Common/StyledForm.styled.jsx
@@ -8,11 +8,21 @@ export const StyledForm = styled.form`
 
   /* input border */
   .css-19mk8g1-MuiInputBase-root-MuiFilledInput-root:after {
+    -o-border-color: white;
+    -moz-border-color: white;
+    -ms-border-color: white;
+    -webkit-border-color: white;
+    -icab-border-color: white;
     border-color: white;
   }
 
   /* input label */
   .css-11qvwfa-MuiFormLabel-root-MuiInputLabel-root.Mui-focused {
+    -o-color: white;
+    -moz-color: white;
+    -ms-color: white;
+    -webkit-color: white;
+    -icab-color: white;
     color: white;
   }
 
